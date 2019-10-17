bda38402ee0da9c469ee1d3e96bfa93d40a57e92
BennieKoops <56158309+BennieKoops@users.noreply.github.com>
2019-10-17 13:58:41 +0200

    Readme edit 5

--- Assets/JS/Galgje.js
+++ Assets/JS/Galgje.js
@@ -4,32 +4,30 @@ var Woorden = ["woord", "Klein", "galg", "niveau", "geschreven"];
 
 var willekeurig ;
 
+var toonWoord = document.getElementById("woord");
+
 // functie die een willekeurig woord selecteerd
 // function that selects a random word
 function WW (woord) {
     var Wmath = Woorden.length
     var RN = Math.round(Math.random() * --Wmath);
     willekeurig = woord[RN];
-    return willekeurig;
-}
-
-// functie die de plekken voor de letters laat zien
-// function that shows the spaces for the letters
-var toonWoord = document.getElementById("woord")
-function uitkomst() {
     for (let i = 0; i < willekeurig.length; i++) {
         var span = document.createElement('span');
         span.textContent = "_";
         toonWoord.appendChild(span);
     }
+    return willekeurig;
 }
 
 var letter = document.getElementById('letter');
 letter.addEventListener('change', function woordcheck(Lvalue) {
     var Lvalue = letter.value;
+    
     for (let i = 0; i <= willekeurig.length; i++) {
+        var goed = document.getElementsByTagName('span')[i];
         if (Lvalue === willekeurig[i]) {
-            console.log(Lvalue);
+            goed.innerText = Lvalue;
         }
     }
 });
--- README.md
+++ README.md
@@ -5,3 +5,4 @@ edit 1: Eerste upload bestanden - First upload of files.
 edit 2: HTML basis toegevoegd en test of JS en CSS verbonden zijn - Added basic HTML an tested if JS and CSS are connected.
 edit 3: Functies voor selecteren willekeurig woord en voor plaatsen span elementen op de pagina toegevoegd - Added functions for selecting random word and adding span elements to page.
 edit 4: Letter check functie gemaakt, --woorden.length maakt errors, var van gemaakt. Made letter check function, --woorden.length causes errors, put it in var.
+edit 5: uitkomst functie in WW functie gezet, uitlijning veranderd naar midden. Put uitkomst function into WW function, changed aligment to center.

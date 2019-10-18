a78d1ecaa3c2130ccb5e1ff093c1733213697964
BennieKoops <56158309+BennieKoops@users.noreply.github.com>
2019-10-18 10:11:27 +0200

    Readme edit 7

--- Assets/JS/Galgje.js
+++ Assets/JS/Galgje.js
@@ -4,9 +4,10 @@ var Woorden = ["woord", "Klein", "galg", "niveau", "geschreven"];
 
 var willekeurig ;
 var span ;
+var pogingen = 0;
 
 var toonWoord = document.getElementById("woord");
-
+var Galgveld = document.getElementById("Galg");
 // functie die een willekeurig woord selecteerd
 // function that selects a random word
 function WW (woord) {
@@ -19,18 +20,32 @@ function WW (woord) {
         span.textContent = "_";
         toonWoord.appendChild(span);
     }
+    console.log("oplossing " + willekeurig)
     return willekeurig;
 }
-
 // eventListener die reageert op wijziging in input veld
 // eventListener that reacts to input in input field
 var letter = document.getElementById('letter');
-letter.addEventListener('change', function woordcheck() {    
+letter.addEventListener('change', function woordcheck() {   
+    for (let i = 0; i < willekeurig.length; i++) {
+        var komtVoor = letter.value === willekeurig[i];
+        if (komtVoor) {
+            i = willekeurig.length;
+            console.log ("dit is i" + i);
+        }
+        
+    } 
+    console.log ("komtVoor" + komtVoor);
     for (let i = 0; i <= willekeurig.length; i++) {
         var goed = document.getElementsByTagName('span')[i];
         if (letter.value === willekeurig[i]) {
             goed.innerText = letter.value;
         }
+    }
+    if (komtVoor === false) {
+        
+        Galgveld.innerHTML = '<img src="Assets\\galgje' + pogingen + '.png" alt="eerste fout"></img>';        
+        ++pogingen;
     }
     letter.value = null;
 });
@@ -42,6 +57,7 @@ start.addEventListener('click', function () {
     if (toonWoord.hasChildNodes('span')) {
         var test = document.getElementsByTagName('span');
         toonWoord.innerHTML = "";
+        pogingen = 0;
         WW(Woorden);
     } else {
         WW(Woorden);
--- README.md
+++ README.md
@@ -7,3 +7,4 @@ edit 3: Functies voor selecteren willekeurig woord en voor plaatsen span element
 edit 4: Letter check functie gemaakt, --woorden.length maakt errors, var van gemaakt. Made letter check function, --woorden.length causes errors, put it in var.
 edit 5: uitkomst functie in WW functie gezet, uitlijning veranderd naar midden. Put uitkomst function into WW function, changed aligment to center.
 edit 6: Functionele knop toegevoegd die een nieuw spel start. Added functional button to start new game.
+edit 7: Afbeeldingen gemaakt voor galgje, HTML button toegevoegd, Woordcheck functie aangepast zodat deze het galgje bouwt. Made images for hangman, added HTML button, changed Woordcheck function so it builds the hangman.
